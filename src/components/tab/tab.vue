0ab5528c6aef73a7549f88b374d8d51ffbb9360a
Gorgio_Liu666 <15711097909@163.com>
2022-04-28 15:42:39 +0800

    header+tab+common

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class='tab'></div>
+    <div class='tab'>
+        <router-link class="tab-item" to="/recommend">
+            <span class="tab-link">推荐</span>
+        </router-link>
+        <router-link class="tab-item" to="/singer">
+            <span class="tab-link">歌手</span>
+        </router-link>
+        <router-link class="tab-item" to="/rank">
+            <span class="tab-link">排行</span>
+        </router-link>
+        <router-link class="tab-item" to="/search">
+            <span class="tab-link">搜索</span>
+        </router-link>
+    </div>
 </template>
 
 <script>
@@ -14,5 +27,25 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
-
+@import "../../common/scss/variable.scss";
+.tab {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    font-size: $font-size-medium;
+    .tab-item {
+        flex: 1;
+        text-align: center;
+        .tab-link {
+            padding-bottom: 5px;
+            color: $color-text-l;
+        }
+        &.router-link-active {
+            .tab-link {
+                color: $color-theme;
+                border-bottom: 2px solid $color-theme;
+            }
+        }
+    }
+}
 </style>
