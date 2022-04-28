c4b4d131cf8412101339f8f88db7aa3571ba6a84
Gorgio_Liu666 <15711097909@163.com>
2022-04-29 00:20:44 +0800

    banner

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class='loading'>
+        <img src="./loading.gif" width="24" height="24" alt="">
+        <p class="desc">{{title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: '正在载入...'
+        }
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+}
+</script>
+<style lang='scss' scoped>
+@import "../../common/scss/variable.scss";
+.loading {
+    width: 100%;
+    text-align: center;
+    .desc {
+        line-height: 20px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+    }
+}
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,17 +1,56 @@
 <template>
     <div class='recommend' ref="recommend">
-        推荐页面
+        <scroll ref="scroll" class="recommend-content" :data="discList">
+            <div>
+                <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+                    <slider>
+                        <div v-for="(item, index) in recommends" :key="index">
+                            <a :href="item.linkUrl">
+                                <img :src="item.picUrl" class="needsclick" @load="loadImage" alt="">
+                            </a>
+                        </div>
+                    </slider>
+                </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单推荐</h1>
+                    <ul>
+                        <li @click="selectItem(item)" v-for="(item, index) in discList" :key="index" class="item">
+                            <div class="icon">
+                                <img width="60" height="60" v-lazy="item.imgurl" src="" alt="">
+                            </div>
+                            <div class="text">
+                                <h2 class="name"></h2>
+                                <p class="desc"></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="loading-container" v-show="!discList.length">
+                <loading></loading> 
+            </div>
+        </scroll>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Slider from '../../base/slider/slider'
+
+import Loading from '../../base/loading/loading'
+import Scroll from '../../base/scroll/scroll'
 import { getRecommend } from '../../api/recommend'
 import { ERR_OK } from '../../api/config' 
 export default {
-    components: {},
+    components: {
+        Slider,
+        Loading,
+        Scroll
+    },
     data() {
         return {
-
+            recommends: [],
+            discList: []
         };
     },
     created() {
@@ -19,16 +58,79 @@ export default {
     },
     computed: {},
     methods: {
+        loadImage () {
+            if(!this.checkloaded) {
+                
+            }
+        },
         _getRecommend() {
             getRecommend().then(res => {
                 if(res.code === ERR_OK) {
                     console.log(res.data.slider);
+                    this.recommends = res.data.slider
                 }
             })
         }
     }
 }
 </script>
-<style lang='stylus' scoped>
-
+<style lang='scss' scoped>
+@import '../../common/scss/variable.scss';
+.recommend {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    .recommend-content {
+        height: 100%;
+        overflow: hidden;
+        .slider-wrapper {
+            position: relative;
+            width: 100%;
+            overflow: hidden;
+        }
+        .recommend-list {
+            .list-title {
+                height: 65px;
+                line-height: 65px;
+                text-align: center;
+                font-size: $font-size-medium;
+                color: $color-theme;
+            }
+            .item {
+                display: flex;
+                box-sizing: border-box;
+                align-items: center;
+                padding: 0 20px 20px 20px;
+                .icon {
+                    flex: 0 0 60px;
+                    width: 60px;
+                    padding-right: 20px;
+                }
+                .text {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    flex: 1;
+                    line-height: 20px;
+                    overflow: hidden;
+                    font-size: $font-size-medium;
+                    .name {
+                        margin-bottom: 10px;
+                        color: $color-text;
+                    }
+                    .desc {
+                        color: $color-text-d;
+                    }
+                }
+            }
+        }
+        .loading-container {
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
+}
 </style>
