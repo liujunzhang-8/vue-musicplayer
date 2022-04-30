885e5b58e0d08bbb274be148e50242dbeb213639
Gorgio_Liu666 <15711097909@163.com>
2022-04-30 15:50:30 +0800

    singerList

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,249 +1,283 @@
 <template>
-    <scroll @scroll="scroll" :listen-scroll="listenScroll" :probe-type="probeType" :data="data" class="listview" ref="listview">
+  <scroll
+    @scroll="scroll"
+    :listen-scroll="listenScroll"
+    :probe-type="probeType"
+    :data="data"
+    class="listview"
+    ref="listview"
+  >
+    <ul>
+      <li
+        v-for="(group, index) in data"
+        :key="index"
+        class="list-group"
+        ref="listGroup"
+      >
+        <h2 class="list-group-title">{{ group.title }}</h2>
         <ul>
-            <li v-for="(group,index) in data" :key="index" class="list-group" ref="listGroup">
-                <h2 class="list-group-title">{{group.title}}</h2>
-                <ul>
-                    <li @click="selectItem(item)" v-for="(item,index) in group.items" :key="index" class="list-group-item">
-                        <img class="avatar" v-lazy="item.avatar">
-                        <span class="name">{{item.name}}</span>
-                    </li>
-                </ul>
-            </li>
+          <li
+            @click="selectItem(item)"
+            v-for="(item, index) in group.items"
+            :key="index"
+            class="list-group-item"
+          >
+            <img class="avatar" v-lazy="item.avatar" />
+            <span class="name">{{ item.name }}</span>
+          </li>
         </ul>
-        <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove" @touchend.stop>
-            <ul>
-                <li v-for="(item,index) in shortcutList" :data-index="index" class="item" :class="{'current': currentIndex === index}" :key="index">{{item}}</li>
-            </ul>
-        </div>
-        <div class="list-fixed" ref="fixed" v-show="fixedTitle">
-            <div class="fixed-title">{{fixedTitle}}</div>
-        </div>
-        <div v-show="!data.length" class="loading-container">
-            <loading></loading>
-        </div>
-    </scroll>
+      </li>
+    </ul>
+    <div
+      class="list-shortcut"
+      @touchstart.stop.prevent="onShortcutTouchStart"
+      @touchmove.stop.prevent="onShortcutTouchMove"
+      @touchend.stop
+    >
+      <ul>
+        <li
+          v-for="(item, index) in shortcutList"
+          :data-index="index"
+          class="item"
+          :class="{ current: currentIndex === index }"
+          :key="index"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{ fixedTitle }}</div>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
+  </scroll>
 </template>
 
 <script>
-import Scroll from '../../base/scroll/scroll'
-import Loading from '../../base/loading/loading'
-import { getData } from '../../common/js/dom'
+import Scroll from "../../base/scroll/scroll";
+import Loading from "../../base/loading/loading";
+import { getData } from "../../common/js/dom";
 
-const TITLE_HEIGHT = 30
-const ANCHOR_HEIGHT = 30
+const TITLE_HEIGHT = 30;
+const ANCHOR_HEIGHT = 30;
 export default {
-    components: {
-        Scroll,
-        Loading
+  components: {
+    Scroll,
+    Loading,
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
     },
-    props: {
-        data: {
-            type: Array,
-            default: () => []
-        }
+  },
+  data() {
+    return {
+      scrollY: -1,
+      currentIndex: 0,
+      diff: -1,
+    };
+  },
+  computed: {
+    shortcutList() {
+      let shortcut = [];
+      if (this.data.length) {
+        return this.data.map((group) => {
+          return (shortcut = group.title.substr(0, 1));
+        });
+      }
+      return shortcut;
     },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return "";
+      }
+      return this.data[this.currentIndex]
+        ? this.data[this.currentIndex].title
+        : "";
+    },
+  },
+  created() {
+    this.probeType = 3;
+    this.listenScroll = true;
+    this.touch = {};
+    this.listHeight = [];
+  },
+  mounted() {
+    console.log(this.data, "获取歌手数据");
+  },
+  methods: {
+    selectItem(item) {
+      this.$emit("select", item);
+    },
+    onShortcutTouchStart(e) {
+      let anchorIndex = getData(e.target, "index");
+      let firstTouch = e.touches[0];
+      // 开始选择时的值
+      this.touch.y1 = firstTouch.pageY;
+      this.touch.anchorIndex = anchorIndex;
+
+      this._scrollTo(anchorIndex);
+    },
+    onShortcutTouchMove(e) {
+      let firstTouch = e.touches[0];
+      // 结束时选择的值
+      this.touch.y2 = firstTouch.pageY;
+      // 锚点偏移量
+      let delta = ((this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT) | 0;
+      let anchorIndex = parseInt(this.touch.anchorIndex) + delta;
+
+      this._scrollTo(anchorIndex);
+    },
+    refresh() {
+      this.$refs.listview.refresh();
+    },
+    scroll(pos) {
+      this.scrollY = pos.y;
+    },
+    _calculateHeight() {
+      this.listHeight = [];
+      const list = this.$refs.listGroup;
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < list.length; i++) {
+        let item = list[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    },
+    _scrollTo(index) {
+        // index 为右侧的快速入口的位置
+      // 点击右侧滚动条上下空白时
+      if (!index && index !== 0) {
+        return;
+      }
+      if (index < 0) {
+        index = 0;
+      } else if (index > this.listHeight.length - 2) { // 最后一个元素
+        index = this.listHeight.length - 2;
+      }
+      this.scrollY = -this.listHeight[index];
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0);
+    },
+  },
+  watch: {
     data() {
-        return {
-            scrollY: -1,
-            currentIndex: 0,
-            diff: -1
-        };
+      setTimeout(() => {
+        this._calculateHeight();
+      }, 20);
     },
-    computed: {
-        shortcutList() {
-            let shortcut = []
-            if(this.data.length) {
-                return this.data.map((group) => {
-                    return shortcut = group.title.substr(0, 1)
-                })
-            }
-            return shortcut
-        },
-        fixedTitle() {
-            if(this.scrollY > 0) {
-                return ''
-            }
-            return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+    scrollY(newY) {
+      const listHeight = this.listHeight;
+      // 当滚动到顶部, newY > 0
+      if (newY > 0) {
+        this.currentIndex = 0;
+        return;
+      }
+      // 在中间部分滚动
+      for (let i = 0; i < listHeight.length - 1; i++) {
+        let height1 = listHeight[i];
+        let height2 = listHeight[i + 1];
+        if (-newY >= height1 && -newY < height2) {
+          this.currentIndex = i;
+          this.diff = height2 + newY;
+          return;
         }
+      }
+      // 当滚动到底部，且-newY大于最后一个元素的上限
+      this.currentIndex = listHeight.length - 2;
     },
-    created() {
-        this.probeType = 3
-        this.listenScroll = true
-        this.touch = {}
-        this.listHeight = []
+    diff(newVal) {
+        console.log('diff', newVal);
+      let fixedTop =
+        newVal > 0 && newVal < TITLE_HEIGHT ? newVal - TITLE_HEIGHT : 0;
+      if (this.fixedTop === fixedTop) {
+        return;
+      }
+      this.fixedTop = fixedTop;
+      this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`;
     },
-    mounted() {
-        console.log(this.data, '获取歌手数据');
-    },
-    methods: {
-        selectItem(item) {
-            this.$emit('select', item)
-        },
-        onShortcutTouchStart(e) {
-            console.log(e);
-            let anchorIndex = getData(e.target, 'index')
-            let firstTouch = e.touches[0]
-            // 开始选择时的值
-            this.touch.y1 = firstTouch.pageY
-            this.touch.anchorIndex = anchorIndex
-
-            this._scrollTo(anchorIndex)
-        },
-        onShortcutTouchMove(e) {
-            console.log(e);
-            let firstTouch = e.touches[0]
-            // 结束时选择的值
-            this.touch.y2 = firstTouch.pageY
-            // 锚点偏移量
-            let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
-            let anchorIndex = parseInt(this.touch.anchorIndex) + delta
-
-            this._scrollTo(anchorIndex)
-        },
-        refresh() {
-            this.$refs.listview.refresh()
-        },
-        scroll(pos) {
-            this.scrollY = pos.y
-        }, 
-        _calculateHeight() {
-            this.listHeight = []
-            const list = this.$refs.listGroup
-            let height = 0
-            this.listHeight.push(height)
-            for(let i = 0; i < list.length; i++) {
-                let item = list[i]
-                height += item.clientHeight
-                this.listHeight.push(height)  
-            }
-        },
-        _scrollTo(index) {
-            if(!index && index !== 0) {
-                return
-            }
-            if(index < 0) {
-                index = 0
-            } else if (index > this.listHeight.length - 2) {
-                index = this.listHeight.length - 2
-            }
-            this.scrollY = -this.listHeight[index]
-            this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
-        }
-    },
-    watch: {
-        data() {
-            setTimeout(() => {
-                this._calculateHeight()
-            }, 20)
-        },
-        scrollY(newY) {
-            const listHeight = this.listHeight
-            // 当滚动到顶部, newY > 0 
-            if(newY > 0) {
-                this.currentIndex = 0
-                return
-            }
-            // 在中间部分滚动
-            for(let i=0; i<listHeight.length -1; i++) {
-                let height1 = listHeight[i]
-                let height2 = listHeight[i + 1]
-                if(-newY >= height1 && -newY < height2) {
-                    this.currentIndex = i
-                    this.diff = height2 + newY
-                    return
-                }
-            }
-            // 当滚动到底部，且-newY大于最后一个元素的上限
-            this.currentIndex = listHeight.length - 2
-        },
-        diff(newVal) {
-            let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
-            if(this.fixedTop === fixedTop) {
-                return
-            }
-            this.fixedTop = fixedTop
-            this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
-        }
-    }
-}
+  },
+};
 </script>
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 @import "../../common/scss/variable.scss";
 
 .listview {
-    position: relative;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+  .list-group {
+    padding-bottom: 30px;
+    .list-group-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
+    }
+    .list-group-item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+  .list-shortcut {
+    position: absolute;
+    z-index: 30;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    padding: 20px 0;
+    border-radius: 10px;
+    text-align: center;
+    background: $color-background-d;
+    font-family: Helvetica;
+    .item {
+      padding: 3px;
+      line-height: 1;
+      color: $color-text-l;
+      font-size: $font-size-small;
+      &.current {
+        color: $color-theme;
+      }
+    }
+  }
+  .list-fixed {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
-    height: 100%;
-    overflow: hidden;
-    background: $color-background;
-    .list-group {
-        padding-bottom: 30px;
-        .list-group-title {
-            height: 30px;
-            line-height: 30px;
-            padding-left: 20px;
-            font-size: $font-size-small;
-            color: $color-text-l;
-            background: $color-highlight-background;
-        }
-        .list-group-item {
-            display: flex;
-            align-items: center;
-            padding: 20px 0 0 30px;
-            .avatar {
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-            }
-            .name {
-                margin-left: 20px;
-                color: $color-text-l;
-                font-size: $font-size-medium;
-            }
-        }
+    .fixed-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
     }
-    .list-shortcut {
-        position: absolute;
-        z-index: 30;
-        right: 0;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 20px;
-        padding: 20px 0;
-        border-radius: 10px;
-        text-align: center;
-        background: $color-background-d;
-        font-family: Helvetica;
-        .item {
-            padding: 3px;
-            line-height: 1;
-            color: $color-text-l;
-            font-size: $font-size-small;
-            &.current {
-                color: $color-theme;
-            }
-        } 
-    }
-    .list-fixed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        .fixed-title {
-            height: 30px;
-            line-height: 30px;
-            padding-left: 20px;
-            font-size: $font-size-small;
-            color: $color-text-l;
-            background: $color-highlight-background;
-        }
-    }
-    .loading-container {
-        position: absolute;
-        width: 100%;
-        top: 50%;
-        transform: translateY(-50%);
-    }
+  }
+  .loading-container {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 }
 </style>
