8baefea83a2b2cc5c8bfc17d1eeeaa6332da8c68
Gorgio_Liu666 <15711097909@163.com>
2022-05-03 12:13:49 +0800

    开发播放器页面

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class='progress-bar'>
-
+    <div class='progress-bar' ref="progressBar" @click="progressClick">
+        <div class="bar-inner">
+            <div class="progress" ref="progress"></div>
+            <div class="progress-btn-wrapper" ref="progressBtn" @touchstart.prevent="progressTouchStart" @touchmove.prevent="progressTouchMove" @touchend="progressTouchEnd">
+                <div class="progress-btn"></div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import { prefixStyle } from '../../common/js/dom';
+
+const progressBtnWidth = 16
+const transform = prefixStyle('transform')
 export default {
+    props: {
+        percent: {
+            type: Number,
+            default: 0
+        }
+    },
+    created() {
+        this.touch = {}
+    },
     components: {},
     data() {
         return {
@@ -14,8 +31,76 @@ export default {
         };
     },
     computed: {},
+    methods: {
+        progressTouchStart(e) {
+
+        },
+        progressTouchMove(e) {
+
+        },
+        progressTouchEnd(e) {
+            this.touch.initiated = false
+            this._triggerPercent()
+        },
+        progressClick(e) {
+            const rect = this.$refs.progressBar.getBoundingClientRect()
+            const offsetWidth = e.pageX - rect.left
+            this._offset(offsetWidth)
+            this._triggerPercent()
+        },
+        _triggerPercent() {
+            const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
+            const percent = this.$refs.progress.clientWidth / barWidth
+            this.$emit('percentChange', percent)
+        },
+        _offset(offsetWidth) {
+            this.$refs.progress.style.width = `${offsetWidth}px`
+            this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px, 0,0)`
+        }
+    },
+    watch: {
+        percent(newPercent) {
+            if(newPercent >= 0 && !this.touch.initiated) {
+                const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
+                const offsetWidth = newPercent * barWidth
+                this._offset(offsetWidth)
+            }   
+        }
+    }
 }
 </script>
 <style lang='scss' scoped>
-
+@import "../../common/scss/variable.scss";
+.progress-bar {
+    height: 30px;
+    .bar-inner {
+        position: relative;
+        top: 13px;
+        height: 4px;
+        background: rgba(0, 0, 0, 0.3);
+        .progress {
+            position: absolute;
+            height: 100%;
+            background: $color-theme;
+        }
+        .progress-btn-wrapper {
+            position: absolute;
+            left: -8px;
+            top: -13px;
+            width: 30px;
+            height: 30px;
+            .progress-btn {
+                position: relative;
+                top: 7px;
+                left: 7px;
+                box-sizing: border-box;
+                width: 16px;
+                height: 16px;
+                border: 3px solid $color-text;
+                border-radius: 50%;
+                background: $color-theme;
+            }
+        }
+    }
+}
 </style>
