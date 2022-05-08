3af01ff27e4f60511e6f3007ad90725e261fbe7c
Gorgio_Liu666 <15711097909@163.com>
2022-05-09 00:07:36 +0800

    feat:排行榜

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,13 +1,43 @@
 <template>
-    <div class='rank'></div>
+    <div class='rank' ref="rank">
+        <scroll :data="topList" class="toplist" ref="toplist">
+            <ul>
+                <li @click="selectItem(item)" class="item" v-for="(item,index) in topList" :key="index">
+                    <div class="icon">
+                        <img width="100" height="100" v-lazy="item.picUrl" />
+                    </div>
+                    <ul class="songlist">
+                        <li class="song" v-for="(song,index) in item.songList" :key="index">
+                            <span>{{index + 1}}</span>
+                            <span>{{song.songname}} - {{song.singername}}</span>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="loading-container" v-show="!topList.length">
+                <loading></loading>
+            </div>
+        </scroll>
+        <<router-view></router-view>
+    </div>
 </template>
 
 <script>
+import Scroll from '../../base/scroll/scroll'
+import Loading from '../../base/loading/loading'
+import { getTopList } from '../../api/rank'
+import { ERR_OK } from '../../api/config'
+import { playlistMixin } from '../../common/js/mixin'
+import { mapMutations } from 'vuex'
 export default {
-    components: {},
+    mixins: [playlistMixin],
+    components: {
+        Scroll,
+        Loading
+    },
     data() {
         return {
-
+            topList: []
         };
     },
     computed: {},
