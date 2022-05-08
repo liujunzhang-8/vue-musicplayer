3c0eb26158a6855f315436dfef542a32ba241574
Gorgio_Liu666 <15711097909@163.com>
2022-05-08 10:37:26 +0800

    rank Api

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -23,7 +23,7 @@ const mutations = {
     [types.SET_CURRENT_INDEX](state, index) {
         state.currentIndex = index
     },
-    [types.SET_DISC](state, disc) {
+    [types.SET_DISC](state, disc) {  
         state.disc = disc
     },
     [types.SET_TOP_LIST](state, topList) {
