70835e8eea0a88ef1a2a87a19de81438548190f8
Gorgio_Liu666 <15711097909@163.com>
2022-05-09 22:07:16 +0800

    feat:rank

--- src/components/top-list/top-list.vue
+++ src/components/top-list/top-list.vue
@@ -1,18 +1,69 @@
 <template>
-    <div class='top-list'></div>
+    <transition name="slide">
+        <music-list :rank="rank" :title="title" :bg-image="bgImage" :song="songs"></music-list>
+    </transition>
 </template>
 
 <script>
+import MusicList from '../../components/music-list/music-list'
+import { getMusicList } from '../../api/rank'
+import { ERR_OK } from '../../api/config'
+import { mapGetters } from 'vuex'
+import { createSong } from '../../common/js/song'
 export default {
-    components: {},
+    components: {
+        MusicList
+    },
     data() {
         return {
-
+            songs: [],
+            rank: true
         };
     },
-    computed: {},
+    computed: {
+        title() {
+            return this.topList.topTitle
+        },
+        bgImage() {
+            if(this.songs.length) {
+                return this.songs[0].image
+            }
+            return ''
+        },
+        ...mapGetters([
+            'topList'
+        ])
+    },
+    methods: {
+        _getMusicList() {
+            if(!this.topList.id) {
+                this.$router.push('/rank')
+                return
+            }
+            getMusicList(this.topList.id).then(res => {
+                if(res.code === ERR_OK) {
+                    this.songs = this._normalizeSongs(res.songlist)
+                }
+            })
+        },
+        _normalizeSongs(list) {
+            let ret = []
+            list.forEach(item => {
+                const musicData = item.data
+                if(musicData.songid && musicData.albummid) {
+                    ret.push(createSong(musicData))
+                }
+            })
+            return ret
+        }
+    }
 }
 </script>
 <style lang='scss' scoped>
-
+.slide-enter-active, .slide-leave-active {
+    transition: all 0.3s ease;
+}
+.slide-enter, .slide-leave-to {
+    transform: translate3d(100%, 0, 0);
+}
 </style>
