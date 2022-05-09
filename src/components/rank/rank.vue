8736c81788122203e71fc2645af46a79b45d0eed
Gorgio_Liu666 <15711097909@163.com>
2022-05-09 21:47:02 +0800

    feat:rank

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -18,7 +18,7 @@
                 <loading></loading>
             </div>
         </scroll>
-        <<router-view></router-view>
+        <router-view></router-view>
     </div>
 </template>
 
@@ -41,8 +41,88 @@ export default {
         };
     },
     computed: {},
+    created() {
+        this._getTopList()
+    },
+    methods: {
+        handlePlaylist(playlist) {
+            const bottom = playlist.length > 0 ? '60px' : ''
+            this.$refs.rank.style.bottom = bottom
+            this.$refs.topList.refresh()
+        },
+        selectItem(item) {
+            this.$router.push({
+                path: `/rank/${item.id}`
+            })
+            this.setTopList(item)
+        },
+        _getTopList() {
+            getTopList().then(res => {
+                if(res.code === ERR_OK) {
+                    this.topList = res.data.topList
+                }
+            })
+        },
+        ...mapMutations({
+            setTopList: 'SET_TOP_LIST'
+        })
+    },
+    watch: {
+        topList() {
+            setTimeout(() => {
+                this.$Lazyload.lazyLoadHandler()
+            }, 20)
+        }
+    }
 }
 </script>
 <style lang='scss' scoped>
-
+@import "../../common/scss/variable.scss";
+@import "../../common/scss/mixin.scss";
+.rank {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    .toplist {
+        height: 100%;
+        overflow: hidden;
+        .item {
+            display: flex;
+            margin: 0 20px;
+            padding-top: 20px;
+            height: 100px;
+            &:last-child {
+                padding-bottom: 20px;
+            }
+            .icon {
+                flex: 0 0 100px;
+                width: 100px;
+                height: 100px;
+            }
+            .songlist {
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding: 0 20px;
+                height: 100px;
+                overflow: hidden;
+                background: $color-highlight-background;
+                color: $color-text-d;
+                font-size: $font-size-small;
+                .song {
+                    // no-wrap()
+                    line-height: 26px;
+                }
+            }
+        }
+        .loading-container {
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
+}
 </style>
