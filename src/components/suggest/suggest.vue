671cdba9eebf40bc210dfd53823ef4efc8286545
Gorgio_Liu666 <15711097909@163.com>
2022-05-11 22:21:38 +0800

    no-result

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class='suggest'></div>
+    <scroll class='suggest' ref="suggest" :data="result" :pullup="pullup" :beforeScroll="beforeScroll" @scrollToEnd="searchMore" @beforeScroll="listScroll"></scroll>
 </template>
 
 <script>
+import Scroll from '../../base/scroll/scroll'
 export default {
-    components: {},
+    components: {
+        Scroll
+    },
     data() {
         return {
 
